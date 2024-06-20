4442752d1f63a1145caf2ae177a5d0268fd32ecb
glennhkm <glenn@mhs.usk.ac.id>
2024-06-20 12:30:25 +0700

    feat: add suspense boundary for usesearchparams

--- app/(pages)/(user)/layout.jsx
+++ app/(pages)/(user)/layout.jsx
@@ -2,16 +2,19 @@
 
 import Image from "next/legacy/image";
 import { usePathname } from "next/navigation";
+import { Suspense } from "react";
 
 const MenuLayout = ({ children }) => {
 const pathname = usePathname();
 const lastPathname = pathname.split('/').pop();
 
 return (
-    <div className={`w-screen relative h-full px-20 bg-gradient-to-b from-[#151515] to-main_bg/60 via-main_bg/60 ${lastPathname === 'menu' ? 'via-90% to-10% py-40' : 'via-30% to-70% py-[9.6rem]'}`}>
-        <div className="absolute top-0 left-0 -z-[1] w-screen h-full overflow-hidden bg-[url('/images/pattern.jpg')] bg-cover opacity-10"></div>
-        {children}
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+        <div className={`w-screen relative h-full px-20 bg-gradient-to-b from-[#151515] to-main_bg/60 via-main_bg/60 ${lastPathname === 'menu' ? 'via-90% to-10% py-40' : 'via-30% to-70% py-[9.6rem]'}`}>
+            <div className="absolute top-0 left-0 -z-[1] w-screen h-full overflow-hidden bg-[url('/images/pattern.jpg')] bg-cover opacity-10"></div>
+            {children}
+        </div>
+    </Suspense>
 );
 };
 
